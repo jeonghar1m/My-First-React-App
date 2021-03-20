102a0d6f08313b98758092b16e6acfcf95a8a706
Harim Jeong <harim97714@naver.com>
2021-03-21 04:12:26 +0900

    Update Top.js

--- src/components/Top.js
+++ src/components/Top.js
@@ -3,7 +3,7 @@ class Top extends Component {
     render() {
         return (
             <header>
-                <span id="logo"><a href="index.php">정하림</a></span>
+                <span id="logo"><a href="../">정하림</a></span>
             </header>
         );
     }
