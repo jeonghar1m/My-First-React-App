a930beb9696b1750258ff48feaa863f0b75c9c4c
Harim Jeong <harim97714@naver.com>
2021-03-21 23:51:55 +0900

    update

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ class Contents extends Component {
         <section>
             <h1>자기소개</h1>
             <hr />
-            <div id="face"><img src="img/face.png" width="150px" /></div>
+            <div id="face"><img src="src/face.png" width="150px" /></div>
             저는 홍익대학교 게임소프트웨어전공에 재학하고 있는 15학번 정하림입니다.
         </section>
       );
